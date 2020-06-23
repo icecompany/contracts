e817be04042ea3636bf8e822a1263d9d5b352fb1
Anton <asharikov@icecompany.org>
2020-06-23 16:16:50 +0300

    Thematics export

--- administrator/components/com_contracts/models/thematics.php
+++ administrator/components/com_contracts/models/thematics.php
@@ -16,6 +16,7 @@ class ContractsModelThematics extends ListModel
         parent::__construct($config);
         $input = JFactory::getApplication()->input;
         $this->contractID = $config['contractID'] ?? 0;
+        $this->contractIDs = $config['contractIDs'] ?? [];
         $this->export = ($input->getString('format', 'html') === 'html') ? false : true;
         $this->return = ContractsHelper::getReturnUrl();
     }
@@ -26,8 +27,17 @@ class ContractsModelThematics extends ListModel
 
         $query
             ->select("ct.thematicID")
-            ->from("#__mkv_contract_thematics ct")
-            ->where("ct.contractID = {$this->_db->q($this->contractID)}");
+            ->from("#__mkv_contract_thematics ct");
+        if ($this->contractID > 0) {
+            $query->where("ct.contractID = {$this->_db->q($this->contractID)}");
+        }
+        if (!empty($this->contractIDs)) {
+            $ids = implode(', ', $this->contractIDs);
+            $query
+                ->select("ct.contractID, t.title")
+                ->leftJoin("#__mkv_thematics t on t.id = ct.thematicID")
+                ->where("ct.contractID in ({$ids})");
+        }
 
         $this->setState('list.limit', 0);
 
@@ -39,7 +49,10 @@ class ContractsModelThematics extends ListModel
         $items = parent::getItems();
         $result = [];
         foreach ($items as $item) {
-            $result[] = $item->thematicID;
+            if ($this->contractID > 0) $result[] = $item->thematicID;
+            if (!empty($this->contractIDs)) {
+                $result[$item->contractID][] = $item->title;
+            }
         }
         return $result;
     }
@@ -57,5 +70,5 @@ class ContractsModelThematics extends ListModel
         return parent::getStoreId($id);
     }
 
-    private $export, $contractID, $return;
+    private $export, $contractID, $contractIDs, $return;
 }
