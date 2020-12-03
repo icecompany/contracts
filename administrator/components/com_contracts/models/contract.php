6a07d2a27e364f14ed7b8e25d82d8304e8670894
xakepok@xakepok.com <44duP_yx579>
2020-12-03 12:58:40 +0300

    Not set null number and dat if decline

--- administrator/components/com_contracts/models/contract.php
+++ administrator/components/com_contracts/models/contract.php
@@ -444,12 +444,6 @@ class ContractsModelContract extends AdminModel {
                 }
             }
         }
-        //Обнуляем номер и дату договора, если не отказной статус
-        if ($table->status == '0') {
-            $table->number = NULL;
-            $table->number_free = NULL;
-            $table->dat = NULL;
-        }
 
         parent::prepareTable($table);
     }
